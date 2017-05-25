ee9d13ae22ff4bde43411638f9f83ddfffdb18ca
Nate <nategbrown1@gmail.com>
2017-05-25 15:21:38 -0400

    modified site color theme

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
 angular.module("MyApp", ["ngMaterial"])
 
+  .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('blue-grey');
+  })
   .controller("TabOne", function($scope) {
     $scope.title = "Count Upwards";
   });
